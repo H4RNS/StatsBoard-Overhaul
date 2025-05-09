56f3f78ee5104d34d282265f76f06c4ab99728a3
H4RNS <h4rnsfreakybritishman@gmail.com>
2025-05-09 23:37:07 +0100

    Main Push
    First Push

---
+++ StatsBoard-Overhaul/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using BepInEx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StatsBoard_Overhaul
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        GameObject _statsBoardobj;
+        Text _PlayerHeader;
+        void Start() => GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+        void OnEnable() => HarmonyPatches.ApplyHarmonyPatches();
+        void OnDisable() => HarmonyPatches.RemoveHarmonyPatches();
+        void OnGameInitialized()
+        {
+            try
+            {
+                if (_statsBoardobj == null)
+                {
+                    _statsBoardobj = Instantiate(InitialiseBoard("StatsBoard-Overhaul.Resources.StatsBoard").LoadAsset<GameObject>("StatsBoard"));
+                }
+                else
+                {
+                    Debug.Log($"[StatsBoard]: SUCCESSFULLY, Initialized StatsBoard");
+                }
+            }
+            catch
+            {
+                Debug.LogError($"[StatsBoard]: ERROR, when Initialing StatsBoard");
+            }
+            //_PlayerHeader = _statsBoardobj.transform.Find("").gameObject.GetComponent<Text>();
+        }
+        void ApplyContent()
+        {
+            //_PlayerHeader.text = PhotonNetwork.LocalPlayer.NickName;
+        }
+        void Update()
+        {
+            ApplyContent();
+        }
+        public AssetBundle InitialiseBoard(string path)
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            var assetBundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return assetBundle;
+        }
+    }
+}
---
+++ StatsBoard-Overhaul/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace StatsBoard_Overhaul
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.h4rns.gorillatag.statsboard-overhaul";
+        public const string Name = "StatsBoard_Overhaul";
+        public const string Version = "1.0.0";
+    }
+}
